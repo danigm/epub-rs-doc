d8be9675f7a71fee4b2569b2284a0381245b7ed0
Daniel García Moreno <danigm@wadobo.com>
2017-07-31 22:23:42 +0200

    v1.0.5 release

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["epub"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='epub/doc/struct.DocError.html' title='epub::doc::DocError'>DocError</a>",];
+implementors["epub"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"epub/doc/struct.DocError.html\" title=\"struct epub::doc::DocError\">DocError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
